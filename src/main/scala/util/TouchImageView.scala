46b3e2b5d07f24d015e6ebcb23ddeaec77bc8727
François-Xavier Thomas <fx.thomas@gmail.com>
2013-04-23 17:49:15 +0200

    [dev] Refactored TouchImageView

--- src/main/scala/util/TouchImageView.scala
+++ src/main/scala/util/TouchImageView.scala
@@ -8,7 +8,34 @@ import android.util._
 
 import org.scaloid.common._
 
+
 class TouchImageView(ctx: Context, attrs: AttributeSet, defStyle: Int) extends ImageView(ctx, attrs, defStyle) {
+
+  case class Point(x: Float, y: Float) {
+    lazy val normalized = {
+      // Compute the current image<->imageview transformation matrix
+      val d = getDrawable
+      val m = new Matrix
+      val drawableRect = new RectF(0, 0, d.getIntrinsicWidth, d.getIntrinsicHeight)
+      val viewRect = new RectF(0, 0, getWidth, getHeight)
+      m set getImageMatrix
+      m.setRectToRect(drawableRect, viewRect, Matrix.ScaleToFit.CENTER)
+      m invert m
+
+      // Transform the points
+      val a = Array(x,y)
+      m mapPoints a
+
+      // Return the normalized Point
+      Point(
+        a(0) / d.getIntrinsicWidth,
+        a(1) / d.getIntrinsicHeight
+      )
+    }
+
+    lazy val inside = normalized.x >= 0 && normalized.x <= 1 &&
+                      normalized.y >= 0 && normalized.y <= 1
+  }
 
   def this(ctx: Context) = this(ctx, null, 0)
   def this(ctx: Context, attrs: AttributeSet) = this(ctx, attrs, 0)
@@ -17,63 +44,36 @@ class TouchImageView(ctx: Context, attrs: AttributeSet, defStyle: Int) extends I
   var onSelectHandler: Option[(Float, Float) => Boolean] = None
 
   // Current coordinates
-  var last_inside = false
-  var last_x = 0.f
-  var last_y = 0.f
-  var current_x = 0.f
-  var current_y = 0.f
+  var last_point = Point(0, 0)
+  var finger_down = false
 
   // Long press on the screen adds a new interaction at that position
   this onTouch {
     (v: View, ev: MotionEvent) => {
-      if (ev.getActionMasked != MotionEvent.ACTION_DOWN &&
-          ev.getActionMasked != MotionEvent.ACTION_MOVE) {
-        // We're outside now
-        last_inside = false
+      // True if the finger is down
+      finger_down  = !(ev.getActionMasked != MotionEvent.ACTION_DOWN &&
+                       ev.getActionMasked != MotionEvent.ACTION_MOVE)
 
-      } else {
-        // Store the points
-        last_x = ev.getX
-        last_y = ev.getY
+      // Last touched point
+      last_point = Point(ev.getX, ev.getY)
 
-        // Create the image transformation matrix
-        val d = getDrawable
-        val m = new Matrix
-        val drawableRect = new RectF(0, 0, d.getIntrinsicWidth, d.getIntrinsicHeight)
-        val viewRect = new RectF(0, 0, getWidth, getHeight)
-        m.set(getImageMatrix)
-        m.setRectToRect(drawableRect, viewRect, Matrix.ScaleToFit.CENTER)
-        m.invert(m)
-
-        // Convert the points inside the image
-        val points = Array(ev.getX, ev.getY)
-        m.mapPoints(points)
-
-        // Compute normalized coordinates
-        current_x = points(0) / d.getIntrinsicWidth
-        current_y = points(1) / d.getIntrinsicHeight
-
-        // Update the rendering if we touched inside
-        last_inside = current_x >= 0 && current_x <= 1 &&
-                      current_y >= 0 && current_y <= 1
-      }
+      // Invalidate the view
+      invalidate
 
       // Only continue if we touched inside the image
-      invalidate
-      !last_inside
+      !finger_down || !last_point.inside
     }
   }
 
   this onLongClick {
     (v: View) => {
-      if (last_inside) {
-        last_inside = false
-        invalidate
-        onSelectHandler match {
-          case Some(f) => f(current_x, current_y)
-          case None => false
+      onSelectHandler match {
+        case Some(f) => {
+          val n = last_point.normalized
+          f(n.x, n.y)
         }
-      } else false
+        case None => false
+      }
     }
   }
 
@@ -82,15 +82,15 @@ class TouchImageView(ctx: Context, attrs: AttributeSet, defStyle: Int) extends I
     super.onDraw(canvas)
 
     // If we have a touch, then use it to draw
-    if (last_inside) {
+    if (finger_down) {
       val p = new Paint
       p.setColor(Color.parseColor("#4433B5E5"))
       p.setStyle(Paint.Style.FILL)
-      canvas.drawCircle(last_x, last_y, 50, p)
+      canvas.drawCircle(last_point.x, last_point.y, 50, p)
 
       p.setColor(Color.parseColor("#FF33B5E5"))
       p.setStyle(Paint.Style.STROKE)
-      canvas.drawCircle(last_x, last_y, 50, p)
+      canvas.drawCircle(last_point.x, last_point.y, 50, p)
     }
   }
 
